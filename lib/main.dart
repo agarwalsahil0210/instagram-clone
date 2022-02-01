2c37ac77f56264bdf18b056df6cf63eb83a1e3eb
Sahil Agarwal <agarwalsahil0210@gmail.com>
2022-02-01 13:47:18 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: Scaffold(body: Text('Flutter Demo Home Page')),
+    );
+  }
+}
